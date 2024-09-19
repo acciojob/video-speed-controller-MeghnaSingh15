1e48a33680467a71dbfe18ccfac31c75fc9cc47d
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-19 23:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const speed = document.querySelector('.speed');
+const speedBar = document.querySelector('.speed-bar');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+const minSpeed = 0.5;
+const maxSpeed = 4;
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function handleSpeedChange(e) {
+  const y = e.pageY - speed.offsetTop;
+  const percent = y / speed.offsetHeight;
+  const playbackRate = percent * (maxSpeed - minSpeed) + minSpeed;
+  video.playbackRate = playbackRate;
+  speedBar.style.height = `${percent * 100}%`;
+  speedBar.textContent = `${playbackRate.toFixed(2)}×`;
+}
+
+// Event listener for mouse movements on the speed bar
+speed.addEventListener('mousemove', handleSpeedChange);
+speed.addEventListener('click', handleSpeedChange);
